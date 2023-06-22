da19787966baa2b6c4dfe13f8fec3d06520c667f
johnsontopno <66691981+johnsontopno@users.noreply.github.com>
2023-06-22 09:21:03 +0000

    Bootstrap Add

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+{
+  'id':1,
+  'title':'Data Analyst',
+  'location':'Chandigarh, India',
+  'salary':'Rs.1,00,000'
+},
+{
+  'id':2,
+  'title':'Data Scientist',
+  'location':'Delhi, India',
+  # 'salary':'Rs.2,50,000'
+},
+{
+  'id':3,
+  'title':'Frontend Engineer',
+  'location':'Remote',
+  'salary':'Rs.15,00,000'
+},
+{
+  'id':4,
+  'title':'backend Engineer',
+  'location':'San Francisco , USA',
+  'salary':'$ 120,000'
+}
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS,
+                        company_name='Johnson')
 
 # print(__name__)
 if __name__ == "__main__":
